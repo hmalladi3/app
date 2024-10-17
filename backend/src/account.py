0425455fe5c62b9d77a14caf67ac4a939e688447
hmalladi3 <hmalladi3@gmail.com>
2024-10-17 00:16:54 -0500

    updated app structure

--- backend/src/account.py
+++ backend/src/account.py
@@ -1,55 +1,81 @@
-class Service:
-    def __init__(self, name:str, desc:str, price:float):
-        self.name = name
-        self.desc = desc
-        self.price = price
+from db import db
 
-class Review:
-    def __init__(self, title:str, body:str, stars:float, usr:str):
-        self.title = title
-        self.body = body
-        self.stars = stars
-        self.usr = usr
-
-class Account():
-    # pic:str # profile pic
-    isComplete:bool = False
-    isVerified:bool = False
-
-    def __init__(self, usr:str, pwd:str):
-        self.usr: str = usr # a unique identifier used to query the db
-        self.pwd: str = pwd
-
-    def login(usr:str, pwd:str):
+class Service: # TODO
+    def __init__(self, name:str, desc:str, price:float, usr:str):
+        # add to db
+        # ensure the name is unique within the user
         return
 
-    def addPhoto(self, file):
-        # if photo exists replace it
-        # else add the file as pfp
-        self.isComplete = True
-
-    def addService(self):
-        # create the service
-        # add to db
+    def getService(self):
+        # get the service from the database
+        return
+    
+    def updateService(self):
+        # update the service in the database
+        # if name is changed, ensure the new name is unique within the user
         return
 
     def removeService(self):
         # remove the service from the database
         return
 
-    def addReview(self):
-        # add the review to the database
+class Review: # TODO
+    def __init__(self, title:str, body:str, stars:float, usr:str):
+        # add to db
         return
-
+    
+    def getReview(self):
+        # get the review from the database
+        return
+    
+    def updateReview(self):
+        # update the review in the database
+        return
+    
     def removeReview(self):
         # remove the review from the database
         return
 
+class Account():
+    # pfp stored in ../media/<self.usr>/pfp.jpg
+    # media stored in ../media/<self.usr>/media folder
+
+    def __init__(self, usr:str, pwd:str):
+        # add to db
+        # ensure the username is unique
+        return
+
+    def getAccount(self) -> dict:
+        # get the account info from the database
+        return
+    
+    def updateUsername(self, newUsr:str):
+        # update the username in the database
+        # ensure the new username is unique
+        return
+
+    def updatePassword(self, newPwd:str):
+        # update the password in the database
+        return
+    
+    def removeAccount(self):
+        # remove the account from the database
+        return
+
+    def login(usr:str, pwd:str) -> bool:
+        # check if the username and password are correct
+        return
+
+    def addPhoto(self, file):
+        # add/replace the file as pfp
+        # completed=True in the db
+        return
+
     def addMedia(self):
-        # add the media to the database
+        # add/replace the media to the media folder
         return
 
     def removeMedia(self):
-        # remove the media from the database
+        # remove the media from the media folder
         return
 
--- backend/src/main.py
+++ backend/src/main.py
@@ -2,7 +2,36 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 from uuid import UUID, uuid4
 
+from db import db, add_column
+from account import Account, Service, Review
+
+from math import radians, sin, cos, sqrt, atan2
+
 app = FastAPI()
+
+# cols: usr:str, pwd:str, complete:bool, verified:bool, loc:(lat, long)
+db.create_table("Accounts")
+# cols: usr:str, name:str, desc:str, price:float
+db.create_table("Services")
+# cols: usr:str, stars:float, title:str, body:str
+db.create_table("Reviews")
+
+# helper function finding the distance between two points
+def dist(loc1:tuple[float, float], loc2:tuple[float, float]):
+    # haversine formula
+    R = 3959  # Earth's radius in miles
+
+    lat1, lon1 = radians(loc1[0]), radians(loc1[1])
+    lat2, lon2 = radians(loc2[0]), radians(loc2[1])
+
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = sin(dlat/2)**2 + cos(lat1) * cos(lat2) * sin(dlon/2)**2
+    c = 2 * atan2(sqrt(a), sqrt(1-a))
+
+    return R * c
+
 
 @app.get("/")
 def read():
